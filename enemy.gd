3b799b161e67e9c287baeda7e81f9c486f11bbe3
PyrrhicsDev <singsofspring@gmail.com>
2025-01-11 11:23:18 -0600

    a7
    a7

--- enemy.gd
+++ enemy.gd
@@ -1,13 +1,14 @@
 extends CharacterBody2D
-#test push
+
 var rng = RandomNumberGenerator.new()
+#testing webhook
 func _ready():
 	$enemySprite2D/AnimationPlayer.play('idleEnemy')
 	$"../Timer".wait_time = 2
 	$"../Timer".start() 
 
 func _on_timer_timeout() -> void:
-	$"../Timer".wait_time = rng.randf_range(1, 1.5)
+	$"../Timer".wait_time = rng.rand_range(1, 5)
 	var my_random_number = rng.randi_range(1, 5)
 	if my_random_number == 1:
 		$enemySprite2D/AnimationPlayer.play('idleEnemy')
